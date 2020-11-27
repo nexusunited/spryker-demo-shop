95db02b11661ec243507f0440c97637e262b490e
Rafał Wesołowski <r.wesolowski@nexus-united.com>
2020-11-27 10:40:33 +0100

    init

---
+++ src/Pyz/Service/PriceProduct/PriceProductDependencyProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Service\PriceProduct;
+
+use Spryker\Service\PriceProduct\PriceProductDependencyProvider as SprykerPriceProductDependencyProvider;
+use Spryker\Service\PriceProductOfferStorage\Plugin\PriceProduct\ProductOfferPriceProductFilterPlugin;
+use Spryker\Service\PriceProductOfferVolume\Plugin\PriceProductOffer\PriceProductOfferVolumeFilterPlugin;
+use Spryker\Service\PriceProductVolume\Plugin\PriceProductExtension\PriceProductVolumeFilterPlugin;
+use Spryker\Service\ProductConfigurationStorage\Plugin\PriceProduct\ProductConfigurationPriceProductFilterPlugin;
+use Spryker\Service\ProductConfigurationStorage\Plugin\PriceProduct\ProductConfigurationPriceProductVolumeFilterPlugin;
+
+class PriceProductDependencyProvider extends SprykerPriceProductDependencyProvider
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return \Spryker\Service\PriceProductExtension\Dependency\Plugin\PriceProductFilterPluginInterface[]
+     */
+    protected function getPriceProductDecisionPlugins(): array
+    {
+        return array_merge([
+            new ProductConfigurationPriceProductFilterPlugin(),
+            new ProductConfigurationPriceProductVolumeFilterPlugin(),
+            new ProductOfferPriceProductFilterPlugin(),
+            new PriceProductOfferVolumeFilterPlugin(),
+            new PriceProductVolumeFilterPlugin(),
+        ], parent::getPriceProductDecisionPlugins());
+    }
+}
